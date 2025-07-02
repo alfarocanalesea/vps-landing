73da89f8d32e29516d079ab2cb3d605479bf884f
alfarocanalesea <alf.can.es.al@gmail.com>
2025-07-02 10:11:43 -0600

    A

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// @ts-check
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
-
-export default {
-  preview: {
-    allowedHosts: ['vps.theonix.mx'],
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    preview: {
+      host: true,
+      port: 4321,
+      allowedHosts: ['vps.theonix.mx'],
+    },
   },
-};
+});
